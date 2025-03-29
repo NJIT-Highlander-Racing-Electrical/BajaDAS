a62c13d36063cb537f84fd7b33fd7ec6017a95e8
ahuegler <alexhuegler@gmail.com>
2025-03-29 16:07:46 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,14 @@
 #define TX_GPIO_NUM 25  // Connects to CTX
 #define RX_GPIO_NUM 26  // Connects to CRX
 
-#include <Arduino.h>
+// Pin 13 - Switch
+// Pin 34 - Button 2
+// Pin 35 - Button 1
+
 #include <math.h>
+#include <string>
+#include <iostream>
+#include <Arduino.h>
 #include <Wire.h>
 #include "FS.h"
 #include "SD.h"
@@ -17,44 +23,25 @@
 #include <HardwareSerial.h>
 #include <BajaCAN.h>
 
-// data variables
-int cvtPrimaryRPM = 0;
-int cvtSecondaryRPM = 0;
-int cvtTemp = false;
-
-int gasPedalDepression = 0;
-int brakePedalDepression = 0;
-int steeringWheelPosition = 0;
-
-int wheelSpeed1 = 0; // there's no wheelSpeed2...
-
-int fuelLevel = 0;
-
-bool bmsLowPowerWarning = false;
-
-float gpsLatitude = 0.0;
-float gpsLongitude = 0.0;
-
 String hourString = "";
 String minuteString = "";
 String secondString = "";
-
-int timeHour = 0;
-int timeMinute = 0;
-int timeSecond = 0;
 
 String dateString = "";
 String dayString = "";
 String yearString = "";
 
-int dateMonth = 0;
-int dateDay = 0;
-int dateYear = 0;
+// buttons & switches
+const int buttonPin1 = 35;
+const int buttonPin2 = 34;
+const int switchPin = 13;
 
-// MicroSD logging on/off
-bool sdLoggingEnabled = true;
-// const int LoggingModeSwitch = 13;
 bool dasError = false;
+
+// battery variables
+int batteryPin = 33;
+float batVoltageDividerRatio = 0.23255813953;
+float batteryVoltage;
 
 // time from program start in seconds
 float t;
@@ -71,6 +58,7 @@ const uint8_t SDCARD_CS = 5;
 // gps initialization variables
 String latitude, longitude;
 String latitudeDecimal, longitudeDecimal;
+String gpsStatus;
 HardwareSerial GPS_Serial(2);
 bool hasFix = false;
 int sats = 0;
@@ -78,10 +66,6 @@ int sats = 0;
 // LSM9DS1 variables
 Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(XG_CS, M_CS);
 sensors_event_t a, m, g, temp;
-
-// last observed drive state 
-String lastState;
-String lastDasState;
 
 // log file setup
 File logFile;
@@ -93,10 +77,11 @@ void setupLSM();
 void setupGPS();
 // void setupLoggingMode();
 void readLSM();
-String readLoggingMode();
+void updateBatteryPercentage();
 void readGPS();
 
 void parseGPGGA(String data);
+void parseGPRMC(String data);
 String convertToDecimalDegrees(const String& coordinate, bool isLatitude);
 
 void logSerial();
@@ -105,6 +90,10 @@ void logSD();
 void setNextAvailableFilePath();
 
 void setup() {
+
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
+  pinMode(switchPin, INPUT_PULLUP);
 
   // start serial and wait for it to connect
   Serial.begin(115200);
@@ -115,9 +104,11 @@ void setup() {
   // setup connections to various modules
   setupSD();
   setupLSM();
-  pinMode(13, INPUT_PULLUP); 
-  readLoggingMode();
   setupGPS();
+
+  // setup CAN
+  setupCAN(DAS);
+
   Serial.println ("Finishing Setup");
 }
 
@@ -136,9 +127,7 @@ void loop() {
 
   readLSM();
   readGPS();
-
-  // observe drive state
-  lastDasState = readLoggingMode();
+  updateBatteryPercentage();
 
   // log data to microsd card and serial connection
   logSD();
@@ -234,27 +223,11 @@ void setupGPS() {
   Serial.println("Found GPS");
 }
 
-/* void setupLoggingMode() {
-  Serial.println("Reading DAS switch");
-  pinMode(13, INPUT_PULLUP);
-  if (digitalRead(13) == HIGH) {
-    sdLoggingEnabled = false;
-    Serial.println("DAS RECORDING DISABLED");
-  }
-  else {
-    sdLoggingEnabled = true;
-    Serial.println("DAS RECORDING ENABLED");
-  }
-} */
-
-void logSD() {
-  if (sdLoggingEnabled) {
+void logSD() { // logging switch is being moved to dashboard, logging on/off will come over CAN
+  if (true) {
     logFile = SD.open(logFilePath, FILE_APPEND);
     if (logFile) {
-      if(!logFile.printf("%s, %s, %s, %f, %s, %s, %f, %f, %f, %f, %f, %f, %s, %s, %i, %i, %i, %i, %i, %i, %i, %i\n", hourString, minuteString, secondString, 
-                        t, lastState, lastDasState, a.acceleration.x, a.acceleration.y, a.acceleration.z,
-                        g.gyro.x, g.gyro.y, g.gyro.z,
-                        latitudeDecimal.c_str(), longitudeDecimal.c_str(), hasFix, sats, cvtPrimaryRPM, cvtSecondaryRPM, cvtTemp, gasPedalDepression, brakePedalDepression, wheelSpeed1))
+      if(!logFile.printf("%s, %s, %s, %f, %s, %s, %f, %f, %f, %f, %f, %f, %s, %s, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i\n", hourString, minuteString, secondString, t, "placeholder", "placeholder", a.acceleration.x, a.acceleration.y, a.acceleration.z, g.gyro.x, g.gyro.y, g.gyro.z, latitudeDecimal.c_str(), longitudeDecimal.c_str(), hasFix, sats, primaryRPM, secondaryRPM, primaryTemperature, secondaryTemperature, gasPedalPercentage, brakePedalPercentage, frontRightWheelRPM, frontLeftWheelRPM, rearRightWheelRPM, rearLeftWheelRPM))
       {
         Serial.println("Logging Failed.");
       }
@@ -273,6 +246,14 @@ void readLSM() {
   /* Get a new sensor event */ 
   t = millis()/1000.0;
   lsm.getEvent(&a, &m, &g, &temp);
+
+  accelerationX = a.acceleration.x;
+  accelerationY = a.acceleration.y;
+  accelerationZ = a.acceleration.z;
+
+  gyroscopePitch = g.gyro.x;
+  gyroscopeYaw = g.gyro.y;
+  gyroscopeRoll = g.gyro.z;
 }
 
 void logSerial() { // Write all values to the console with tabs in between them
@@ -305,33 +286,58 @@ void logSerial() { // Write all values to the console with tabs in between them
 
   // GPS data
   Serial.print("\t");
-  Serial.print(latitudeDecimal);          // GPS Latitude
+  Serial.print(gpsLatitude);              // GPS Latitude
   Serial.print("\t");
-  Serial.print(longitudeDecimal);         // GPS Longitude
+  Serial.print(gpsLatitude);              // GPS Longitude
   Serial.print("\t");
-  Serial.print(hasFix);            // GPS Fix
+  Serial.print(hasFix);                   // GPS Fix
   Serial.print("\t");
-  Serial.print(sats);          // GPS Satalites
+  Serial.print(sats);                     // GPS Satellites
 
   // CVT Data
   Serial.print("\t");
-  Serial.print(cvtPrimaryRPM);
+  Serial.print(primaryRPM);
   Serial.print("\t");
-  Serial.print(cvtSecondaryRPM);
+  Serial.print(secondaryRPM);
   Serial.print("\t");
-  Serial.print(cvtTemp);
+  Serial.print(primaryTemperature);
+  Serial.print("\t");
+  Serial.print(secondaryTemperature);
 
   // Pedal Data
   Serial.print("\t");
-  Serial.print(gasPedalDepression);
+  Serial.print(gasPedalPercentage);
   Serial.print("\t");
-  Serial.print(brakePedalDepression);
-
-  //Wheel Speed Data
-  Serial.print("\t");
-  Serial.print(wheelSpeed1);
+  Serial.print(brakePedalPercentage);
 
   Serial.println(); // Finish with a newline
+}
+
+void updateBatteryPercentage() {
+  batteryVoltage = analogRead(batteryPin) / 4095 * 3.3 / batVoltageDividerRatio;
+  if (batteryVoltage >= 12.60) batteryPercentage = 100;
+  else if (batteryVoltage >= 12.54) batteryPercentage = 95;
+  else if (batteryVoltage >= 12.48) batteryPercentage = 90;
+  else if (batteryVoltage >= 12.42) batteryPercentage = 85;
+  else if (batteryVoltage >= 12.36) batteryPercentage = 80;
+  else if (batteryVoltage >= 12.30) batteryPercentage = 75;
+  else if (batteryVoltage >= 12.18) batteryPercentage = 70;
+  else if (batteryVoltage >= 12.06) batteryPercentage = 65;
+  else if (batteryVoltage >= 11.94) batteryPercentage = 60;
+  else if (batteryVoltage >= 11.82) batteryPercentage = 55;
+  else if (batteryVoltage >= 11.70) batteryPercentage = 50;
+  else if (batteryVoltage >= 11.58) batteryPercentage = 45;
+  else if (batteryVoltage >= 11.46) batteryPercentage = 40;
+  else if (batteryVoltage >= 11.34) batteryPercentage = 35;
+  else if (batteryVoltage >= 11.22) batteryPercentage = 30;
+  else if (batteryVoltage >= 11.10) batteryPercentage = 25;
+  else if (batteryVoltage >= 10.98) batteryPercentage = 20;
+  else if (batteryVoltage >= 10.86) batteryPercentage = 15;
+  else if (batteryVoltage >= 10.74) batteryPercentage = 10;
+  else if (batteryVoltage >= 10.62) batteryPercentage = 5;
+  else if (batteryVoltage >= 10.50) batteryPercentage = 0;
+  else batteryPercentage = 0;  // Below 10.5V is over-discharged
+  Serial.println(batteryPercentage);
 }
 
 void readGPS() {
@@ -360,22 +366,6 @@ void setNextAvailableFilePath() {
     }
 }
 
-String readLoggingMode() {
- if (digitalRead(13) == HIGH) {
-    sdLoggingEnabled = false;
-  }
-  else {
-    sdLoggingEnabled = true;
-  }
-
-  if (sdLoggingEnabled) {
-    return "SD LOG ON";
-  }
-  else {
-    return "SD LOG OFF";
-  }
-}
-
 //Function to convert string NMEA data from GPS into usable decimal degrees format
 String convertToDecimalDegrees(const String& coordinate, bool isLatitude) {
     int degreesLength = isLatitude ? 2 : 3; // Determine the number of degrees (DD or DDD)
@@ -391,7 +381,7 @@ String convertToDecimalDegrees(const String& coordinate, bool isLatitude) {
     return String(decimalDegrees, 6); // 6 decimal places
 }
 
-// Take the raw gps data and convert
+// Take the raw gps data and convert - $GPGGA
 void parseGPGGA(String data) {
   if (data.startsWith("$GPGGA")) {
     // Split the data using commas
@@ -422,29 +412,29 @@ void parseGPGGA(String data) {
     hourString = timedat.substring(0,2); 
 
     int hours = hourString.toInt(); // Convert the string to an integer
-    hours -= 5;
+    hours -= 4;
     // Handle cases where the offset causes the hour to go negative
     if (hours < 0) {
-        hours += 24; // Wrap around to previous day
+      hours += 24; // Wrap around to previous day
     }
 
     if (hours == 0) {
-      timeHour = 12; // Midnight
+      gpsTimeHour = 12; // Midnight
     } else if (hours > 12) {
-      timeHour = hours - 12; // Afternoon/evening
+      gpsTimeHour = hours - 12; // Afternoon/evening
     } else {
-      timeHour = hours; // Morning
+      gpsTimeHour = hours; // Morning
     }
 
-    hourString = String(timeHour);
-     if (timeHour < 10) {
+    hourString = String(gpsTimeHour);
+     if (gpsTimeHour < 10) {
       hourString = "0" + hourString;
     }
 
     minuteString = timedat.substring(2,4);
-    timeMinute = minuteString.toInt();
+    gpsTimeMinute = minuteString.toInt();
     secondString = timedat.substring(4,6);
-    timeSecond = secondString.toInt();
+    gpsTimeSecond = secondString.toInt();
 
     // Extract latitude and longitude if available
     if (fieldCount > 4 && fields[2].length() > 0 && fields[4].length() > 0) {
@@ -456,21 +446,115 @@ void parseGPGGA(String data) {
     }
 
     // Convert latitude and longitude NMEA strings into decimal degrees strings (for Google Maps, GPS Visualizer, etc)
- if (latitude.length() > 0 && longitude.length() > 0) {
-        // Convert NMEA coordinates to decimal degrees strings
-        latitudeDecimal = convertToDecimalDegrees(latitude, 1);
-        longitudeDecimal = "-" + convertToDecimalDegrees(longitude, 0);
+    if (latitude.length() > 0 && longitude.length() > 0) {
+      // Convert NMEA coordinates to decimal degrees strings
+      latitudeDecimal = convertToDecimalDegrees(latitude, 1);
+      gpsLatitude = latitudeDecimal.toFloat();
+      longitudeDecimal = "-" + convertToDecimalDegrees(longitude, 0);
+      gpsLongitude = longitudeDecimal.toFloat();
     }
 
     // Extract fix status and satellite count
     if (fieldCount > 6) {
       int fix = fields[6].toInt();
       hasFix = (fix > 0);
-
       sats = fields[7].toInt();
     } else {
       hasFix = false;
       sats = 0;
+    }
+  }
+}
+
+// Take the raw gps data and convert - $GPRMC
+void parseGPRMC(String data) {
+  if (data.startsWith("$GPRMC")) {
+    // Split the data using commas
+    int maxFields = 12;  // GPGGA has a maximum of 15 fields
+    String fields[maxFields];
+    int fieldCount = 0;
+    
+    int start = 0;
+    int pos = data.indexOf(',');
+    while (pos != -1 && fieldCount < maxFields) {
+      fields[fieldCount] = data.substring(start, pos);
+      fieldCount++;
+      
+      start = pos + 1;
+      pos = data.indexOf(',', start);
+    }
+    if (start < data.length() && fieldCount < maxFields) {
+      fields[fieldCount++] = data.substring(start);
+    }
+
+    // Extract time information if available
+    if (fieldCount > 0 && fields[1].length() > 0) {
+      timedat = fields[1];
+    } else {
+      timedat = "err";
+    }
+
+    hourString = timedat.substring(0,2); 
+
+    int hours = hourString.toInt(); // Convert the string to an integer
+    hours -= 5;
+    // Handle cases where the offset causes the hour to go negative
+    if (hours < 0) {
+      hours += 24; // Wrap around to previous day
+    }
+
+    if (hours == 0) {
+      gpsTimeHour = 12; // Midnight
+    } else if (hours > 12) {
+      gpsTimeHour = hours - 12; // Afternoon/evening
+    } else {
+      gpsTimeHour = hours; // Morning
+    }
+
+    hourString = String(gpsTimeHour);
+     if (gpsTimeHour < 10) {
+      hourString = "0" + hourString;
+    }
+
+    minuteString = timedat.substring(2,4);
+    gpsTimeMinute = minuteString.toInt();
+    secondString = timedat.substring(4,6);
+    gpsTimeSecond = secondString.toInt();
+
+    // Extract date if available - ddmmyy
+    if (fieldCount > 8 && fields[9].length() > 0) {
+      gpsDateDay = fields[9].substring(0,1).toInt();
+      gpsDateMonth = fields[9].substring(2,3).toInt();
+      gpsDateYear = fields[9].substring(4,5).toInt();
+    } else {
+      gpsDateDay = 99;
+      gpsDateMonth = 99;
+      gpsDateYear = 99;
+    }
+
+    // Extract latitude and longitude if available
+    if (fieldCount > 5 && fields[3].length() > 0 && fields[5].length() > 0) {
+      latitude = fields[3];
+      longitude = fields[5];
+    } else {
+      latitude = "";
+      longitude = "";
+    }
+
+    // Convert latitude and longitude NMEA strings into decimal degrees strings (for Google Maps, GPS Visualizer, etc)
+    if (latitude.length() > 0 && longitude.length() > 0) {
+      // Convert NMEA coordinates to decimal degrees strings
+      latitudeDecimal = convertToDecimalDegrees(latitude, 1);
+      gpsLatitude = latitudeDecimal.toFloat();
+      longitudeDecimal = "-" + convertToDecimalDegrees(longitude, 0);
+      gpsLongitude = longitudeDecimal.toFloat();
+    }
+
+    // Extract fix status and satellite count
+    if (fieldCount > 1) {
+      gpsStatus = fields[2]; // A = ok (ie. fix established), V = invalid (ie. not enough satellites)
+    } else {
+      gpsStatus = "err";
     }
   }
 }
